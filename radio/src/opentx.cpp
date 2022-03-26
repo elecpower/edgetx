20a6ffca7a3e50a90c742fc36f595ea1441d1812
gagarinlg <malte@langermann.net>
2022-03-26 09:32:15 +0100

    fix compiling with internal storage but without sd card for NV14, preparations for other radios

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -1380,7 +1380,9 @@ void opentxClose(uint8_t shutdown)
 #endif
   }
 
+#if defined(SDCARD)
   logsClose();
+#endif
 
   storageFlushCurrentModel();
 
@@ -1415,7 +1417,6 @@ void opentxClose(uint8_t shutdown)
 #endif
 #endif
 
-  sdDone();
   VirtualFS::instance().stop();
 }
 
@@ -1423,13 +1424,12 @@ void opentxResume()
 {
   TRACE("opentxResume");
 
-  sdMount();
+  VirtualFS::instance().restart();
 #if defined(COLORLCD) && defined(LUA)
   // reload widgets
   luaInitThemesAndWidgets();
 #endif
 
-  VirtualFS::instance().restart();
   storageReadAll();
 
 #if defined(COLORLCD)
@@ -1698,7 +1698,7 @@ void opentxInit()
   // storage related stuff, only done if not unexpectedShutdown
   if (!globalData.unexpectedShutdown) {
 
-    VirtualFS& vfs = VirtualFS::instance(); // initialize storage subsystem
+    VirtualFS& vfs __attribute__((unused)) = VirtualFS::instance(); // initialize storage subsystem
 
 #if !defined(COLORLCD)
     if (!vfs.defaultStorageAvailable()) {
@@ -1728,8 +1728,9 @@ void opentxInit()
       }
     }
 #endif
-
+#if defined(SDCARD)
     logsInit();
+#endif
   }
 
 #if defined(EEPROM)
