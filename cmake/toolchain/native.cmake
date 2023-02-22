c4da58cb31ef50d0b3f8f5d8d4e97b95cd179e6a
elecpower <neilh713@tpg.com.au>
2023-02-23 08:00:19 +1100

    Revert to std g++ compiler

--- cmake/toolchain/native.cmake
+++ cmake/toolchain/native.cmake
@@ -2,8 +2,6 @@
 
 set(CMAKE_CXX_STANDARD 17)
 
-set(CMAKE_CXX_COMPILER clazy)
-
 if(APPLE)
   set(CMAKE_C_FLAGS "-Wno-asm-operand-widths -Wno-deprecated-declarations")
   set(CMAKE_CXX_FLAGS "-Wno-asm-operand-widths -Wno-deprecated-declarations")
